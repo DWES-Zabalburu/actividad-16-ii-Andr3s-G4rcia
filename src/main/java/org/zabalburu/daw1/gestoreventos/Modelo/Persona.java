107c560ad7b4cde8752cd3e87d8a5591dfe1b864
andre <andre@mvkiabello_pc>
2025-01-21 17:15:09 +0100

    Clase Persona creada

---
+++ src/main/java/org/zabalburu/daw1/gestoreventos/Modelo/Persona.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.zabalburu.daw1.gestoreventos.Modelo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author DAW1
+ */
+public class Persona {
+    private int id;
+    private Date fnacimiento;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String contraseña;
+    private List<Evento> eventosAsistente = new ArrayList<>();
+    
+    
+}
