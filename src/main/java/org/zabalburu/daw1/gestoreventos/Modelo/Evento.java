a6c97fedacd987111fe1d0e572bb6e7344056420
andre <andre@mvkiabello_pc>
2025-01-22 09:32:45 +0100

    Cambios en las clases

--- src/main/java/org/zabalburu/daw1/gestoreventos/Modelo/Evento.java
+++ src/main/java/org/zabalburu/daw1/gestoreventos/Modelo/Evento.java
@@ -7,16 +7,50 @@ package org.zabalburu.daw1.gestoreventos.Modelo;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  *
  * @author DAW1
  */
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Evento {
+    private static int numEventos=0;
+    @EqualsAndHashCode.Include
     private int id;
     private String descripcion;
     private Date fecha;
     private String lugar;
     private boolean mayorEdad;
-    private List<Persona> personasAsistentes = new ArrayList<>();
+    private double coste;
+    private List<Persona> asistentes = new ArrayList<>();
+
+    public Evento() {
+        Evento.numEventos++;
+        this.id=Evento.numEventos;
+    }
+
+    public Evento(String descripcion, Date fecha, String lugar, boolean mayorEdad, double coste) {
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.lugar = lugar;
+        this.mayorEdad = mayorEdad;
+        this.coste = coste;
+    }
+
+    @Override
+    public String toString() {
+        return "Evento{" + "id=" + id + ", descripcion=" + descripcion + ", fecha=" + fecha + ", lugar=" + lugar + ", mayorEdad=" + mayorEdad + ", coste=" + coste + '}';
+    }
+    
+    
+    
+    
+    
 }
--- src/main/java/org/zabalburu/daw1/gestoreventos/Modelo/Persona.java
+++ src/main/java/org/zabalburu/daw1/gestoreventos/Modelo/Persona.java
@@ -5,22 +5,61 @@
 package org.zabalburu.daw1.gestoreventos.Modelo;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 
 /**
  *
  * @author DAW1
  */
+@Data
+@Setter
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true )
 public class Persona {
+    private static int numPersonas;
+    @EqualsAndHashCode.Include
     private int id;
     private Date fnacimiento;
     private String nombre;
     private String apellido;
     private String dni;
     private String contraseña;
-    private List<Evento> eventosAsistente = new ArrayList<>();
+    private List<Evento> eventos = new ArrayList<>();
+
+    public Persona() {
+        Persona.numPersonas++;
+        this.id=Persona.numPersonas;
+    }
+
+    public Persona(Date fnacimiento, String nombre, String apellido, String dni, String contraseña) {
+        this.fnacimiento = fnacimiento;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.contraseña = contraseña;
+    }
     
+    public boolean isMayorEdad(){
+        GregorianCalendar gcHoy= new GregorianCalendar();
+        GregorianCalendar gcFechaNacimiento=new GregorianCalendar();
+        gcFechaNacimiento.setTime(this.fnacimiento);
+        int añoActual=gcHoy.get(Calendar.YEAR);
+        int añoNacimiento= gcFechaNacimiento.get(Calendar.YEAR);
+        int edad= añoActual - añoNacimiento;
+        // Si no ha cumplido años el dia actual sera MENOR respecto al dia de su nacimiento
+        if(gcHoy.get(Calendar.DAY_OF_YEAR) < gcFechaNacimiento.get(Calendar.DAY_OF_YEAR)){
+            edad--;
+        }
+        return edad>=18;
+    }
     
     
 }
